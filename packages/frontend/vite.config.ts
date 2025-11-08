e19a6aea01e5e001a17bf62431e627e7e1abd9ae
markshawn2020 <shawninjuly@gmail.com>
2025-11-09 01:38:39 +0800

    feat: update code-insepctor

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from "path"
 // @ts-ignore
 import tailwindcss from "@tailwindcss/vite"
-import { codeInspectorPlugin } from '@markshawn/code-inspector-plugin'
+import { codeInspectorPlugin } from 'code-inspector-plugin'
 
 export default defineConfig(({ mode }) => {
 	const isDev = mode === 'development';
